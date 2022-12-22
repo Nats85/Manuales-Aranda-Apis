4ddc12e968ca9c2924a336d9824c49ac36386f16
natalia.sanchez <natalia.sanchez@arandasoft.com>
2022-12-22 18:46:12 -0500

    Update vote-count.js

--- assets/js/modules/vote-count.js
+++ assets/js/modules/vote-count.js
@@ -1,51 +1,12 @@
-// Plugin: https://codepen.io/badurski/pen/RJvJQZ
+var lClicks = 0;
+var dClicks = 0;
 
-// Plugin: https://codepen.io/badurski/pen/RJvJQZ
-class poll {
-	constructor(question, answers, options) {
-	  const defaultOptions = {};
-	  this.options = Object.assign({}, defaultOptions, options);
-	  this.history = [];
-	  this.possibleAnswers = answers;
-	}
-  
-	clear() {
-	  this.history = [];
-	}
-  
-	get results() {
-	  let numberOfVotes = this.history.length,
-	  votesResults = [];
-  
-	  Object.keys(this.possibleAnswers).forEach(answerId => {
-		let answerIdCounter = 0;
-		let voters = [];
-		this.history.forEach(vote => {
-		  if (answerId == vote.id) {
-			answerIdCounter++;
-			voters.push(vote.name);
-		  }
-		});
-		let percentOfAllVotes = answerIdCounter / numberOfVotes * 100;
-		let formatedPercent = isNaN(percentOfAllVotes) ?
-		0 :
-		parseFloat(percentOfAllVotes).
-		toFixed(3).
-		slice(0, -1);
-		votesResults.push({
-		  votes: answerIdCounter,
-		  voters: voters,
-		  percent: formatedPercent });
-  
-	  });
-  
-	  return votesResults;
-	}
-  
-	vote(answerId, name = "Anonymouse") {
-	  if (this.possibleAnswers[answerId]) {
-		let getCurrentDate = new Date().toLocaleString();
-		this.history.push({ id: answerId, name: name, date: getCurrentDate });
-		return true;
-	  } else throw new Error("Incorrect answer's id");
-	}}
+$(".likes").on("click", function(){
+	lClicks += 1;
+    document.getElementById("l-counter").innerHTML = lClicks;
+})
+
+$(".dislikes").on("click", function(){
+	dClicks += 1;
+    document.getElementById("d-counter").innerHTML = dClicks;
+})
