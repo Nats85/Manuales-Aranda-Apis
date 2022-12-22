bd8d8ba50af7df74c3e11f03233a9b92b85d74a8
natalia.sanchez <natalia.sanchez@arandasoft.com>
2022-12-22 15:05:12 -0500

    Update vote-count.js

--- assets/js/modules/vote-count.js
+++ assets/js/modules/vote-count.js
@@ -6,25 +6,26 @@ const q1 = new poll("Will Poland win the footboal match?", {
 });
 
 // Add some randome votes
-for(let i = 0; i<20; i++) {
+for (let i = 0; i < 20; i++) {
 	q1.vote(Math.floor(Math.random() * (1 - 0 + 1)) + 0);
 }
 
 // Poll interface script
-let pollButtons = document.querySelectorAll('.poll-panel-btn'),
-		pollPanel = document.querySelector('.poll-panel');
-pollButtons.forEach(button => {
+let pollButtons = document.querySelectorAll(".poll-panel-btn"),
+	pollPanel = document.querySelector(".poll-panel");
+pollButtons.forEach((button) => {
 	button.onclick = () => {
-		if (button.getAttribute('disabled') != 'disabled') {
+		if (button.getAttribute("disabled") != "disabled") {
 			q1.vote(button.dataset.vote);
-			pollPanel.classList.add('poll-voted');
-			button.classList.add('--user-choice');
-			pollButtons.forEach(b => {
-				b.setAttribute('disabled', 'disabled');
-				let percent = q1.results[b.dataset.vote].percent + '%';
+			pollPanel.classList.add("poll-voted");
+			button.classList.add("--user-choice");
+			pollButtons.forEach((b) => {
+				b.setAttribute("disabled", "disabled");
+				let percent = q1.results[b.dataset.vote].percent + "%";
 				b.style.width = percent;
 				b.dataset.result = percent;
 			});
 		}
 	};
 });
+
