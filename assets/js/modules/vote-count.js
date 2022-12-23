42835e765138c4501613291c5b0798402f645284
natalia.sanchez <natalia.sanchez@arandasoft.com>
2022-12-23 09:20:59 -0500

    Update vote-count.js

--- assets/js/modules/vote-count.js
+++ assets/js/modules/vote-count.js
@@ -1,12 +1,8 @@
-var lClicks = 0;
-var dClicks = 0;
+$(function () {
+  $(".like").click(function () {
+      var input = $(this).find('.qty1');
+      input.val(parseInt(input.val())+ 1);
 
-$(".likes").on("click", function(){
-	lClicks += 1;
-    document.getElementById("l-counter").innerHTML = lClicks;
-})
+  });
 
-$(".dislikes").on("click", function(){
-	dClicks += 1;
-    document.getElementById("d-counter").innerHTML = dClicks;
-})
+});
